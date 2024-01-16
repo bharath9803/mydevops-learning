10a2341721470ae4fb9747edc7364f08f992d709
bharath9803 <133229131+bharath9803@users.noreply.github.com>
2024-01-16 19:36:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,10 @@ const port = 3000;
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define a route for the root URL
-app.get('/', (req, res) => {
-    // You can send an HTML file or render a template here
-    res.send('<h1>Hello, this is the root page!</h1>');
-});
+
+// ... other routes and code
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
