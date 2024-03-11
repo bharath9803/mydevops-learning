e74d1b918b4b8742938334ca09de5b1a141b16fd
bharath9803 <133229131+bharath9803@users.noreply.github.com>
2024-03-11 20:13:29 +0530

    Delete Simplewebsite directory

--- Simplewebsite/Simpleapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.greet, name='Home-page'),
-]
--- Simplewebsite/Simpleapp/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def greet(request):
-    return render(request,'Simpleapp/index.html')
